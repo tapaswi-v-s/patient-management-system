2c99d05daf69452d8b21b1660405c5f8595cc2ba
tapaswi <satyapanthi.t@northeastern.edu>
2024-02-21 15:01:49 -0500

    changed models

--- patient-management-system/src/models/Appointment.java
+++ patient-management-system/src/models/Appointment.java
@@ -10,17 +10,41 @@ public class Appointment {
     Patient patient;
     Date date;
     String timeSlot;
+    String note;
     Hospital hospital;
 
-    public Appointment(Doctor doctor, Patient patient, Date date, String timeSlot, Hospital hospital) {
+    public Appointment(
+            Doctor doctor, Patient patient, Date date,
+            String timeSlot, String note, Hospital hospital) {
         this.doctor = doctor;
         this.patient = patient;
         this.date = date;
         this.timeSlot = timeSlot;
         this.hospital = hospital;
+        this.note = note;
     }
 
     public Patient getPatient() {
         return patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getTimeSlot() {
+        return timeSlot;
+    }
+
+    public Hospital getHospital() {
+        return hospital;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
     }
 }
